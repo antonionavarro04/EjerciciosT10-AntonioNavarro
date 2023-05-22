8c2875b13185fb0e279008c9a40cdb9d428a9071
Navarro <antonio.navarro@iesnervion.es>
2023-05-22 11:51:38 +0200

    Ejercicio 1 Terminado

--- src/ficheros/Ejercicio1.java
+++ src/ficheros/Ejercicio1.java
@@ -2,16 +2,29 @@ package ficheros;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Scanner;
 
 public class Ejercicio1 {
     private static final String ROUTE = "ejercicio1";
 
     public static void main(String[] args) {
+        // ^ Definimos un Scanner
+        Scanner sc;
+
+        // ^ Definimos dos variables, una para la suma total y otra para la media
+        int sumaTotal = 0, i = 0;
+
         try {
-            FileReader fr = new FileReader("out\\" + ROUTE + "\\NumerosReales.txt");
+            sc = new Scanner(new FileReader("out\\" + ROUTE + "\\NumerosReales.txt"));
+
+            while (sc.hasNextInt()) {
+                sumaTotal += sc.nextInt();
+                i++;
+            } sc.close();
         } catch (FileNotFoundException e) {
-            e.printStackTrace();                 
-            Math.random();
+            System.err.printf("El fichero no fue encontrado...\n");
+        } finally {
+            System.out.printf("La suma total es: %d\nLa media aritmética es: %.2f", sumaTotal, (double) sumaTotal / i);
         }
     }
 }
